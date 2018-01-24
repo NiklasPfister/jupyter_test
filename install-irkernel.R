5a17ae9ff22fdadb260d4555a7b9956a7f3cb12d
NiklasPfister <niklas.pfister@stat.math.ethz.ch>
2018-01-24 17:57:02 +0100

    Update install-irkernel.R

--- install-irkernel.R
+++ install-irkernel.R
@@ -1,6 +1,6 @@
 install.packages(c('crayon', 'pbdZMQ', 'devtools'),
                 repos = 'http://cran.us.r-project.org')
-devtools::install_github('IRkernel/IRkernel')
+devtools::install_github(paste0('IRkernel/', c('repr', 'IRdisplay', 'IRkernel')))
 
 
 IRkernel::installspec()
